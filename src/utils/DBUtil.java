5b9ab8a44c103988bd644e0ba132786da8bf29cc
takayuki.ito <taka_chunity_007@outlook.jp>
2020-06-21 13:43:48 +0900

    Modify Index/NewServlet

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -8,7 +8,7 @@ public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
-    public static EntityManager createEntityManageer() {
+    public static EntityManager createEntityManager() {
         return _getEntityManagerFactory().createEntityManager();
     }
 
